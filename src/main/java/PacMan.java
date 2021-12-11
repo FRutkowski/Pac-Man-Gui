1c2a91c7fd222be3eead2e1d3024cab2ad6a9fea
SiwyKot41 <Frutkowski80@gmail.com>
2021-12-11 19:10:53 +0100

    Initial commit

---
+++ src/main/java/PacMan.java
@@ -0,0 +1,9 @@
+import view.MainMenu;
+
+import javax.swing.*;
+
+public class PacMan {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+    }
+}
---
+++ src/main/java/view/MainMenu.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.*;
+
+public class MainMenu extends JPanel {
+
+    private JFrame mainFrame;
+    JButton startButton;
+    JButton settingsButton;
+    JButton topPlayersButton;
+    JButton exitButton;
+
+    public MainMenu() {
+        super();
+    }
+}
